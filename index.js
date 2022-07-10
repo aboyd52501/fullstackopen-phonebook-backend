d76d50ed9a8cf388200c05cd5264af48d54c361a
aboyd52501 <aboyd52501@gmail.com>
2022-07-10 17:28:07 -0500

    3.3 Phonebook backend step 3

--- index.js
+++ index.js
@@ -40,6 +40,15 @@ app.get('/api/persons', (req, res) => {
     res.json(persons);
 });
 
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const person = persons.find(person => person.id === id);
+    if (!person)
+        res.status(404).end();
+    else
+        res.json(person);
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
